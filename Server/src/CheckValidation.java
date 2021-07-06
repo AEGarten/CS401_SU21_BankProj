c9da1e4c049a8a3406460bc55040f556c69efccc
AEGarten <etherjack@gmail.com>
2021-07-05 22:47:35 -0700

    Removed debugging messages

--- Server/src/CheckValidation.java
+++ Server/src/CheckValidation.java
@@ -5,7 +5,6 @@ public class CheckValidation {
 	
 	public boolean validateCheck() {
 		int chances = 1 + rand.nextInt(101);
-System.out.println(chances);
 		
 		if (chances > 95) return false;
 		return true;
