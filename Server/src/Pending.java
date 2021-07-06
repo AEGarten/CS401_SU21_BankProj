e497f3ae9053910a3330585bd9584a3178647af3
AEGarten <etherjack@gmail.com>
2021-07-05 18:05:33 -0700

    Added single attribute Date.

--- Server/src/Pending.java
+++ Server/src/Pending.java
@@ -2,9 +2,6 @@ import java.util.Date;
 
 public class Pending {
 	final int id =  (int) (new Date().getTime() - 1625500000000L); //get long to int
+	Date date = new Date();
 	
-	public static void main(String[] args) {
-	
-	System.out.println(new Date().getTime() - 1625500000000L);
-	}
 }
