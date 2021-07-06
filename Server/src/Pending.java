8ec6be9980b6bfd87cafa17c853958a1c76ec722
Jubiey <odumosudurodoluwa@gmail.com>
2021-07-06 14:43:58 -0700

    Added comment

--- Server/src/Pending.java
+++ Server/src/Pending.java
@@ -2,6 +2,6 @@ import java.util.Date;
 
 public class Pending {
 	final int id =  (int) (new Date().getTime() - 1625500000000L); //get long to int
-	Date date = new Date();
+	Date date = new Date(); //reset
 	
 }
