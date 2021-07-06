a30e4adebad83f5a1c22ddf31e499d2a5841e382
AEGarten <etherjack@gmail.com>
2021-07-06 13:55:18 -0700

    updating from main

--- Server/src/Account.java
+++ Server/src/Account.java
@@ -2,7 +2,7 @@ import java.util.Date;
 
 public class Account {
 	private boolean postiveStatus = false;
-	final int id =  (int) (new Date().getTime() - 1625500000000L); //get long to int
+	final int id = 0;
 	
 	
 }
