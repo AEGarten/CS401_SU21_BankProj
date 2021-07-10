2728b5cc00670ad4a0cc68ac026f0e867fae9190
AEGarten <41935856+AEGarten@users.noreply.github.com>
2021-07-10 13:55:55 -0700

    Update Server/src/Pending.java

--- Server/src/Pending.java
+++ Server/src/Pending.java
@@ -1,7 +1,6 @@
 import java.util.Date;
 
 public class Pending {
-	final int id =  (int) (new Date().getTime() - 1625500000000L); //get long to int
 	Date date = new Date();
 	Money amount;
 	int checkNumber;
