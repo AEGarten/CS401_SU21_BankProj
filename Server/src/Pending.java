75fe088e65a70cb32be33a80a428113fd372fe9b
Jubiey <odumosudurodoluwa@gmail.com>
2021-07-10 10:47:27 -0700

    Completed Pending

--- Server/src/Pending.java
+++ Server/src/Pending.java
@@ -3,5 +3,14 @@ import java.util.Date;
 public class Pending {
 	final int id =  (int) (new Date().getTime() - 1625500000000L); //get long to int
 	Date date = new Date();
+	Money amount;
 	
+	public Pending(Date date, Money amount) {
+		this.date = date;
+		this.amount = amount;
+	}
+	
+	public int getID() {
+		return id;
+	}
 }
