a6360ca04b232f21e5dc4b72dfb1b0f188380c90
Jubiey <odumosudurodoluwa@gmail.com>
2021-07-10 10:52:41 -0700

    Made changes based off the updated Pending class diagram

--- Server/src/Pending.java
+++ Server/src/Pending.java
@@ -4,10 +4,12 @@ public class Pending {
 	final int id =  (int) (new Date().getTime() - 1625500000000L); //get long to int
 	Date date = new Date();
 	Money amount;
+	int checkNumber;
 	
-	public Pending(Date date, Money amount) {
+	public Pending(Date date, Money amount, int checkNumber) {
 		this.date = date;
 		this.amount = amount;
+		this.checkNumber = checkNumber;
 	}
 	
 	public int getID() {
